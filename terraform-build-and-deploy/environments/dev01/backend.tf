af433b13eca56ecdfa14f622ea95bb1e4e6e457f
dpdeepankar <48359697+dpdeepankar@users.noreply.github.com>
2025-12-12 17:39:12 +0530

    Update backend.tf

--- terraform-build-and-deploy/environments/dev01/backend.tf
+++ terraform-build-and-deploy/environments/dev01/backend.tf
@@ -1,4 +1,13 @@
 terraform {
+    backend "azurerm" {
+       resource_group_name = "__TERRAFORM_STATE_RESOURCE_GROUP_NAME__"
+       storage_account_name = "__TERRAFORM_STATE_STORAGE_ACCOUNT_NAME__"
+       container_name = "__TERRAFORM_STATE_CONTAINER_NAME__"
+       key = "__ENVIRONMENT__/01-foundation/terraform.tfstate"
+       use_azuread_auth = true 
+       subscription_id = "__AZURE_SUBSCRIPTION_ID__"
+       tenant_id = "__AZURE_TENANT_ID__"
+    }
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
